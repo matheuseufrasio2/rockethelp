5c1b6c90f2bb0ea7ec50ea006761cf4e4dcc332d
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-07-17 21:34:54 -0300

    initial config

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  VStack,
+  Heading,
+} from 'native-base';
+
+export function SignIn() {
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+    </VStack>
+  );
+}
