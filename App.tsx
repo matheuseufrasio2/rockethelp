5392ad24bcf714903fea2fe0fc383e8ba59a065f
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-07-19 21:18:00 -0300

    create Home screen

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { SignIn } from './src/screens/SignIn';
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +17,7 @@ export default function App() {
         translucent
       />
       
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/Home.tsx
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
+import { ChatTeardropText, SignOut } from 'phosphor-react-native';
+
+import Logo from '../assets/logo_secondary.svg';
+import { Filter } from '../components/Filter';
+import { Order, OrderProps } from '../components/Order';
+import { Button } from '../components/Button';
+
+export function Home() {
+  const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
+  // const [orders, setOrders] = useState<OrderProps[]>([
+  //   {
+  //     id: '123',
+  //     patrimony: '234545232345',
+  //     when: '18/07/2022 às 10:00',
+  //     status: 'open',
+  //   },
+  //   {
+  //     id: '2342345',
+  //     patrimony: '345667565',
+  //     when: '17/04/2021 às 19:00',
+  //     status: 'closed',
+  //   },
+  // ]);
+  const [orders, setOrders] = useState<OrderProps[]>([]);
+
+  const { colors } = useTheme();
+
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+      <HStack
+        w="full"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="gray.600"
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+        />
+
+      </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack
+          w="full"
+          mt={8}
+          mb={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading color="gray.100" >
+            Meus chamados
+          </Heading>
+          <Text color="gray.200">
+            3
+          </Text>
+        </HStack>
+
+        <HStack space={3} mb={8}>
+          <Filter
+            title="Em andamento"
+            type="open"
+            onPress={() => setStatusSelected('open')}
+            isActive={statusSelected === 'open'}
+          />
+
+          <Filter
+            title="Finalizados"
+            type="closed"
+            onPress={() => setStatusSelected('closed')}
+            isActive={statusSelected === 'closed'}
+          />
+        </HStack>
+
+        <FlatList
+          data={orders}
+          keyExtractor={(order) => order.id}
+          renderItem={({ item }) => (
+            <Order key={item.id} data={item} />
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+          ListEmptyComponent={() => (
+            <Center>
+              <ChatTeardropText size={40} color={colors.gray[300]} />
+              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                Você ainda não possui {'\n'}
+                solicitações { statusSelected === 'open' ? 'em andamento' : 'finalizados' }
+              </Text>
+            </Center>
+          )}
+        />
+
+        <Button title="Nova solicitação" />
+      </VStack>
+    </VStack>
+  );
+}
